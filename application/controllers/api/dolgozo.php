5340193fd7230a01e79447bf9f705063ef3f27df
Budaházi Bence <budahazi.bence@petrik.hu>
2022-01-21 15:56:19 +0100

    jwt - bearer auth

--- application/controllers/api/dolgozo.php
+++ application/controllers/api/dolgozo.php
@@ -1,32 +1,38 @@
-<?php 
+<?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require_once APPPATH. 'libraries/REST_Controller.php';
+require_once APPPATH . 'libraries/REST_Controller.php';
 
-class Dolgozo extends REST_Controller {
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
 
-    public function __construct() {
+class Dolgozo extends REST_Controller
+{
+
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('dolgozo_model');
+        $this->check_valid_token();
     }
 
     public function index_get($id = 0)
-    {   
+    {
         $adatok = [];
         $error = false;
         $message = "Dolgozók sikeresen lekérdezve";
         $response_code = REST_Controller::HTTP_OK;
         if ($id == 0) {
             $adatok = $this->dolgozo_model->get_all();
-        } else{
+        } else {
             $adatok = $this->dolgozo_model->get_by_id($id);
             if (is_null($adatok)) {
                 $error = true;
                 $message = "A megadott azonosítóval nem található dolgozó";
                 $adatok = [];
                 $response_code = REST_Controller::HTTP_NOT_FOUND;
-            } else{
+            } else {
                 $adatok = [$adatok];
             }
         }
@@ -51,7 +57,7 @@ class Dolgozo extends REST_Controller {
         $this->form_validation->set_rules('nem', 'Nem', 'trim|required');
         $this->form_validation->set_rules('kor', 'Kor', 'trim|required|numeric|greater_than_equal_to[14]|less_than_equal_to[200]');
         $this->form_validation->set_rules('fizetes', 'Fizetés', 'trim|required|numeric|greater_than_equal_to[80000]|less_than_equal_to[999999]');
-        
+
         if ($this->form_validation->run() == FALSE) {
             $error = true;
             $message = validation_errors();
@@ -64,7 +70,7 @@ class Dolgozo extends REST_Controller {
             $insert_data = $this->post();
             $lehetseges = ['nev', 'nem', 'kor', 'fizetes'];
             foreach ($insert_data as $key => $value) {
-                if (!in_array($key, $lehetseges) ) {
+                if (!in_array($key, $lehetseges)) {
                     unset($insert_data[$key]);
                 }
             }
@@ -72,8 +78,8 @@ class Dolgozo extends REST_Controller {
             $adatok = [$this->dolgozo_model->get_by_id($id)];
             $message = "Új dolgozó sikeresen létrehozva";
         }
-        
-        
+
+
         $data = [
             'adatok' => $adatok,
             'error' => $error,
@@ -94,7 +100,7 @@ class Dolgozo extends REST_Controller {
         $this->form_validation->set_rules('nem', 'Nem', 'trim|required');
         $this->form_validation->set_rules('kor', 'Kor', 'trim|required|numeric|greater_than_equal_to[14]|less_than_equal_to[200]');
         $this->form_validation->set_rules('fizetes', 'Fizetés', 'trim|required|numeric|greater_than_equal_to[80000]|less_than_equal_to[999999]');
-        
+
         if ($this->form_validation->run() == FALSE) {
             $error = true;
             $message = validation_errors();
@@ -107,7 +113,7 @@ class Dolgozo extends REST_Controller {
             $update_data = $this->put();
             $lehetseges = ['nev', 'nem', 'kor', 'fizetes'];
             foreach ($update_data as $key => $value) {
-                if (!in_array($key, $lehetseges) ) {
+                if (!in_array($key, $lehetseges)) {
                     unset($update_data[$key]);
                 }
             }
@@ -121,14 +127,13 @@ class Dolgozo extends REST_Controller {
                 $adatok = [$this->dolgozo_model->get_by_id($id)];
             }
         }
-        
+
         $data = [
             'adatok' => $adatok,
             'error' => $error,
             'message' => $message,
         ];
         $this->response($data, $response_code);
-        
     }
 
     public function index_delete($id)
@@ -137,13 +142,13 @@ class Dolgozo extends REST_Controller {
         $error = false;
         $message = "";
         $response_code = REST_Controller::HTTP_OK;
-        
+
         $success = $this->dolgozo_model->delete($id);
         if ($success == 0) {
             $error = true;
             $message = "A megadott azonosítóval nem található dolgozó";
             $response_code = REST_Controller::HTTP_NOT_FOUND;
-        } else{
+        } else {
             $message = "Dolgozó sikeresen törölve";
         }
 
@@ -155,10 +160,36 @@ class Dolgozo extends REST_Controller {
         $this->response($data, $response_code);
     }
 
+    private function check_valid_token()
+    {
+        $auth_header = $this->input->get_request_header('AUTHORIZATION');
+        $pos = strpos($auth_header, 'Bearer');
+        if (!is_numeric($pos)) {
+            $this->not_permited();
+        }
+        $this->load->model('user_model');
+        $header_data = explode(' ', $auth_header);
+        $jwt = $header_data[1];
+        $key = getenv('JWT_KEY');
+        $user = (array)JWT::decode($jwt, new Key($key, 'HS256'));
+        $user_check = $this->user_model->get_by_id($user['id']);
+        $same = ( count( $user ) == count( $user_check ) && !array_diff( $user, $user_check ) );
+        if (!$same) {
+            $this->not_permited();
+        }
+    }
+
+    private function not_permited()
+    {
+        $response_code = REST_Controller::HTTP_UNAUTHORIZED;
+        $data = [
+            'adatok' => [],
+            'message' => "Nincs megfelelő jogosultság",
+            'error' => true
+        ];
+        $this->response($data, $response_code);
+        die();
+    }
 }
 
 /* End of file Dolgozo.php */
-
-
-
-?>
--- application/controllers/api/user.php
+++ application/controllers/api/user.php
@@ -1,32 +1,34 @@
-<?php 
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once APPPATH . 'libraries/REST_Controller.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+class User extends REST_Controller
+{
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-require_once APPPATH. 'libraries/REST_Controller.php';
-
-class User extends REST_Controller {
-
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('user_model');
     }
 
     public function index_get($id = 0)
-    {   
+    {
+        $this->check_permission_level();
         $adatok = [];
         $error = false;
         $message = "Felhasználók sikeresen lekérdezve";
         $response_code = REST_Controller::HTTP_OK;
         if ($id == 0) {
             $adatok = $this->user_model->get_all();
-        } else{
+        } else {
             $adatok = $this->user_model->get_by_id($id);
             if (is_null($adatok)) {
                 $error = true;
                 $message = "A megadott azonosítóval nem található felhasználó";
                 $adatok = [];
                 $response_code = REST_Controller::HTTP_NOT_FOUND;
-            } else{
+            } else {
                 $adatok = [$adatok];
             }
         }
@@ -50,7 +52,7 @@ class User extends REST_Controller {
         $this->form_validation->set_rules('username', 'Felhasználónév', 'trim|required|is_unique[user.username]', ['is_unique' => "A megadott felhasználónévvel már szerepel felhasználó"]);
         $this->form_validation->set_rules('password', 'Jelszó', 'trim|required');
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[user.email]', ['is_unique' => "A megadott email címmel már szerepel felhasználó"]);
-        
+
         if ($this->form_validation->run() == FALSE) {
             $error = true;
             $message = validation_errors();
@@ -69,7 +71,7 @@ class User extends REST_Controller {
             $adatok = [$this->user_model->get_by_id($id)];
             $message = "Sikeres regisztráció";
         }
-        
+
         $data = [
             'adatok' => $adatok,
             'error' => $error,
@@ -89,7 +91,7 @@ class User extends REST_Controller {
         $this->form_validation->set_data($this->post());
         $this->form_validation->set_rules('username', 'Felhasználónév', 'trim|required');
         $this->form_validation->set_rules('password', 'Jelszó', 'trim|required');
-        
+
         if ($this->form_validation->run() == FALSE) {
             $error = true;
             $message = validation_errors();
@@ -109,14 +111,17 @@ class User extends REST_Controller {
                 $user = $users[0];
                 if (!password_verify($password, $user['password'])) {
                     $response_code = REST_Controller::HTTP_UNAUTHORIZED;
-                    $message = "Hibás felhasználónév vagy jelszó";                    
+                    $message = "Hibás felhasználónév vagy jelszó";
                 } else {
-                    $adatok = $users;
+                    $key = getenv('JWT_KEY');
+                    $payload = $user;
+                    $jwt = JWT::encode($payload, $key, 'HS256');
+                    $adatok = ['token' => $jwt];
                     $message = "Sikeres bejelentkezés";
                 }
             }
         }
-        
+
         $data = [
             'adatok' => $adatok,
             'error' => $error,
@@ -127,6 +132,7 @@ class User extends REST_Controller {
 
     public function index_put($id)
     {
+        $this->check_permission_level();
         $adatok = [];
         $error = false;
         $message = "";
@@ -137,7 +143,7 @@ class User extends REST_Controller {
         $this->form_validation->set_rules('password', 'Jelszó', 'trim|required');
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[user.email]');
         $this->form_validation->set_rules('permission', 'Jogosultság', 'trim|required|numeric');
-        
+
         if ($this->form_validation->run() == FALSE) {
             $error = true;
             $message = validation_errors();
@@ -153,7 +159,7 @@ class User extends REST_Controller {
             $update_data['permission'] = $this->post('permission');
             $password = $this->post('password');
             $update_data['password'] = password_hash($password, PASSWORD_DEFAULT);
-            
+
             $success = $this->user_model->update($id, $update_data);
             if ($success == 0) {
                 $error = true;
@@ -164,29 +170,29 @@ class User extends REST_Controller {
                 $adatok = [$this->user_model->get_by_id($id)];
             }
         }
-        
+
         $data = [
             'adatok' => $adatok,
             'error' => $error,
             'message' => $message,
         ];
         $this->response($data, $response_code);
-        
     }
 
     public function index_delete($id)
     {
+        $this->check_permission_level();
         $adatok = [];
         $error = false;
         $message = "";
         $response_code = REST_Controller::HTTP_OK;
-        
+
         $success = $this->user_model->delete($id);
         if ($success == 0) {
             $error = true;
             $message = "A megadott azonosítóval nem található felhasználó";
             $response_code = REST_Controller::HTTP_NOT_FOUND;
-        } else{
+        } else {
             $message = "Felhasználó sikeresen törölve";
         }
 
@@ -198,10 +204,38 @@ class User extends REST_Controller {
         $this->response($data, $response_code);
     }
 
+    private function check_permission_level()
+    {
+        $permited = true;
+        $auth_header = $this->input->get_request_header('AUTHORIZATION');
+        $pos = strpos($auth_header, 'Bearer');
+        if (!is_numeric($pos)) {
+            $this->not_permited();
+        }
+        $header_data = explode(' ', $auth_header);
+        $jwt = $header_data[1];
+        $key = getenv('JWT_KEY');
+        $user = (array)JWT::decode($jwt, new Key($key, 'HS256'));
+        if ($user['permission'] < 1) {
+            $this->not_permited();
+        }
+        $user_check = $this->user_model->get_by_id($user['id']);
+        if (empty($user_check) || $user_check['permission'] < 1) {
+            $this->not_permited();
+        }
+    }
+
+    private function not_permited()
+    {
+        $response_code = REST_Controller::HTTP_UNAUTHORIZED;
+        $data = [
+            'adatok' => [],
+            'message' => "Nincs megfelelő jogosultság",
+            'error' => true
+        ];
+        $this->response($data, $response_code);
+        die();
+    }
 }
 
 /* End of file Dolgozo.php */
-
-
-
-?>
